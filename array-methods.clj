d8cc7bf9cf0d7d886acb017ff9281530a033c17d
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2023-05-18 12:39:11 +0530

    array methods

--- array-methods.clj
+++ array-methods.clj
@@ -2,7 +2,7 @@
 
 (def numbers (range))
 
-(def intermidiate-term-generator (fn [[a b]] (vector (+ a b) (+ a b b))))
+(defn intermidiate-term-generator [[a b]] [(+ a b) (+ a b b)])
 
 (defn fib
   [n]
@@ -12,12 +12,13 @@
 
 (defn fact
   [n]
-  (map first 
+  (last (map first 
        (take n 
-             (iterate (fn [[a b]] [(* a b) (+ b 1)]) [1 2]))))
+             (iterate (fn [[a b]] [(* a b) (+ b 1)]) [1 2])))))
+
+
 
 (fact 5)
-;; (defn )
 (take 5 (iterate inc 5))
 ;; => (5 6 7 8 9)
 
